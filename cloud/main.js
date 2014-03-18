e3d3da4941611e0b6d9ec1455833cabef6b69259
Edward Chiang <edwardubuntu@gmail.com>
2014-03-18 18:23:33 +0800

    Add update User stories function

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,46 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+// Update Users stories count
+Parse.Cloud.define("updateUserStoriesCount", function(request, response){
+	var story = Parse.Object.extend("Story");
+	var storiesQuery = new Parse.Query(story);
+	storiesQuery.equalTo("StoryTeller", Parse.User.current());
+	storiesQuery.find({
+		success: function(results) {
+			// Update for user impact
+			var userImpact = Parse.Object.extend("UserImpact");
+			var userImpactQuery = new Parse.Query(userImpact);
+			userImpactQuery.equalTo("User", Parse.User.current());
+			userImpactQuery.find({
+				success: function(userImpacts) {
+					for (var i = 0; i < userImpacts.length; i++) { 
+						var userImpactObject = userImpacts[i];
+						userImpactObject.set("sharedStoriesCount", results.length);
+						userImpactObject.save();
+						
+						response.success("Successfully save " + results.length + " stories count for " + Parse.User.current().get("name") +".");
+					}
+					if (userImpacts.length == 0) {
+						var UserImpact = Parse.Object.extend("UserImpact");
+						var userImpactObject = new UserImpact();
+						
+						userImpactObject.set("User", Parse.User.current());
+						userImpactObject.set("sharedStoriesCount", results.length);
+						userImpactObject.save();
+						
+						response.success("Successfully create new user impact with " + results.length + " stories count for " + Parse.User.current().get("name") +".");
+					}
+				}, 
+				error: function(error) {
+					response.error("Error: " + error.code + " " + error.message);
+				}
+			});
+		}, 
+		error: function(error) {
+			response.error("Error: " + error.code + " " + error.message);
+		}
+	}
+	);
+});
